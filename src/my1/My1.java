bfcc547c2f6e9bcfdee1abf8caf4287b39eb2ec0
pate6281 <pate6281@sheridancollege.ca>
2025-01-17 16:33:38 -0500

    initial

---
+++ src/my1/My1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package my1;
+
+/**
+ *
+ * @author shreyaa010
+ */
+public class My1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("HI");
+        //showing other commit
+    }
+    
+}
